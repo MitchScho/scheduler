f3cae93d4bdb1c50868e606462a2615330a41598
Mitchell Schofield <scho.mitch@gmail.com>
2022-05-25 21:46:01 -0700

    introduction the cypress testing

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -2,25 +2,26 @@ import React from "react";
 import "components/DayListItem.scss";
 import classNames from "classnames";
 
-
 export default function DayListItem(props) {
-
-  const dayClass = classNames('day-list__item', { 'day-list__item--selected': props.selected, 'day-list__item--full': props.spots === 0 });
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
 
   const formatSpots = (spots) => {
     if (!spots || spots === 0) {
-      return "no spots remaining"
+      return "no spots remaining";
     }
     if (spots === 1) {
       return "1 spot remaining";
     }
-    return (`${spots} spots remaining`);
+    return `${spots} spots remaining`;
   };
 
   return (
-      <li onClick={() => props.setDay(props.name)} className={dayClass}>
-        <h2 className="text--regular">{props.name}</h2>
-        <h3 className="text--light">{formatSpots(props.spots)}</h3>
-      </li>
+    <li data-testid="day" onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
   );
-};
+}
