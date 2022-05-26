85475b96a4583e670c03a60c7d687d5216ffb8b4
Mitchell Schofield <scho.mitch@gmail.com>
2022-05-26 12:45:22 -0700

    create edit end to end test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -26,12 +26,25 @@ describe("Appointments", () => {
 
   })
 
-  // it("Should edit an interview", () => {
+  it("Should edit an interview", () => {
 
-  //   cy.get("[alt='Add']")
-  //     .first()
-  //     .click()
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true })
+    
+    cy.get('[data-testid=student-name-input]')
+      .clear()
+      .type('Lydia Miller-Jones')
+    
+    cy.get("[alt='Tori Malcolm']")
+      .click()
+    
+    cy.contains('Save')
+      .click()
 
-  // })
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+
+  })
 
 });
--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,18 +1,15 @@
 
 describe("Navigation", () => {
+  
   it("should visit root", () => {
+
     cy.visit("/");
+
   });
+
   it("should navigate to Tuesday", () => {
+
     cy.visit("/");
-
-  //   cy.contains("[data-testid=day]", "Tuesday")
-  //     .click()
-
-
-  //   cy.get("#list-item-Tuesday")
-  //     .should("have.class", "day-list__item--selected");
-    
     cy.contains("[data-testid=day]", "Tuesday")
       .click()
       .should("have.class", "day-list__item--selected")
